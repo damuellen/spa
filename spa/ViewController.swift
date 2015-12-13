0ad70bb513792658c1aec7eacd8e8ecb1c0d28b9
Daniel Müllenborn <damuellen@aol.com>
2015-12-13 01:36:07 +0100

    Initial Commit

---
+++ spa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spa
+//
+//  Created by Daniel Müllenborn on 13/12/15.
+//  Copyright © 2015 Daniel Müllenborn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
